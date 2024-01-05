f6b058e2d09f58acecbba11bc17f01705bb67523
Matthew Hodgson <matthew@matrix.org>
2024-01-05 16:50:32 +0000

    display the EncryptedFile metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,7 @@
-import './App.css'
-
+import { useState } from 'react';
 import { encryptStreamedAttachment, decryptStreamedAttachment } from 'matrix-encrypt-attachment'
 
-
-async function onFile(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.files && event.target.files[0]) {
-        const file = event.target.files[0];
-        console.log("encrypting file", file);
-        const fileStream = await fileToReadableStream(file);
-        const identityTransform = new TransformStream();
-
-        const info = await encryptStreamedAttachment(fileStream, identityTransform.writable);
-        console.log("Encrypting with info: ", info);
-
-/*
-        const response = await fetch(`https://localhost:8088/matthewtest-${new Date().getTime()}`, {
-            method: 'PUT',
-            body: identityTransform.readable,
-            duplex: 'half',
-            headers: {
-                'Content-Type': file.type,
-            },
-        });
-        console.log(response);
-*/
-
-        const queuingStrategy = new CountQueuingStrategy({ highWaterMark: 1 });
-
-        // incrementally reassemble the image into a blob via a data URL
-        let imageBlob = new Blob();
-        const writableStream = new WritableStream(
-            {
-                write(chunk) {
-                    console.log("WritableStream: Got chunk ", chunk);
-                    imageBlob = new Blob([imageBlob, chunk]);
-                    const imageUrl = URL.createObjectURL(imageBlob);
-                    document.getElementById('outputImage').src = imageUrl;
-                },
-                close() {
-                    console.log("WritableSteam: closed");
-                },
-                abort(err) {
-                    console.log("WritableStream: Sink error:", err);
-                }
-            },
-            queuingStrategy,
-        );
-
-        await decryptStreamedAttachment(identityTransform.readable, writableStream, info);
-    }
-}
+import './App.css'
 
 async function fileToReadableStream(file: File) {
     return new ReadableStream({
@@ -91,6 +43,57 @@ async function fileToReadableStream(file: File) {
 }
 
 function App() {
+    const [info, setInfo] = useState('');
+
+    const onFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
+            console.log("encrypting file", file);
+            const fileStream = await fileToReadableStream(file);
+            const identityTransform = new TransformStream();
+
+            const i = await encryptStreamedAttachment(fileStream, identityTransform.writable);
+            console.log("Encrypting with info: ", i);
+            setInfo(i);
+
+    /*
+            const response = await fetch(`https://localhost:8088/matthewtest-${new Date().getTime()}`, {
+                method: 'PUT',
+                body: identityTransform.readable,
+                duplex: 'half',
+                headers: {
+                    'Content-Type': file.type,
+                },
+            });
+            console.log(response);
+    */
+
+            const queuingStrategy = new CountQueuingStrategy({ highWaterMark: 1 });
+
+            // incrementally reassemble the image into a blob via a data URL
+            let imageBlob = new Blob();
+            const writableStream = new WritableStream(
+                {
+                    write(chunk) {
+                        console.log("WritableStream: Got chunk ", chunk);
+                        imageBlob = new Blob([imageBlob, chunk]);
+                        const imageUrl = URL.createObjectURL(imageBlob);
+                        document.getElementById('outputImage').src = imageUrl;
+                    },
+                    close() {
+                        console.log("WritableSteam: closed");
+                    },
+                    abort(err) {
+                        console.log("WritableStream: Sink error:", err);
+                    }
+                },
+                queuingStrategy,
+            );
+
+            await decryptStreamedAttachment(identityTransform.readable, writableStream, i);
+        }
+    }
+
     return (
         <>
             <form>
@@ -99,6 +102,7 @@ function App() {
             </form>
             <br/>
             <img id="outputImage"/>
+            <pre>{info ? `EncryptedFile = ${JSON.stringify(info, null, 4)}` : ''}</pre>
         </>
     )
 }
